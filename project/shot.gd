af476bd65689e2d3cbd12d5ec5e10dace6470afa
rikedyp <rikedyp@gmail.com>
2018-09-11 17:30:34 +0100

    Bullets aim for where mouse 'was' at time of shooting (space bar)

--- project/shot.gd
+++ project/shot.gd
@@ -6,17 +6,23 @@ extends Area2D
 # var b = "textvar"
 const speed = 300
 var target
+# Declare velocity here for straight line forever bullet
+var velocity = Vector2()
 
 
 func _ready():
 	print("I exitst")
+	# set velocity here for one that goes in straight line forever
+	# oops you gotta set the target here too
+	target = get_global_mouse_position()
+	velocity = -(position - target).normalized()
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
 
 func _process(delta):
-	var velocity = Vector2()
-	target = get_global_mouse_position()
-	velocity = -(position - target).normalized()
+	# (Declare and) Set velocity here for a 'homing' bullet
+	#var velocity = Vector2()
+	#velocity = -(position - target).normalized()
 	#velocity = Vector2(30,0)
 	position += velocity
